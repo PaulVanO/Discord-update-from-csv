d5617efd58e1092cb4ef44f409a8b80355260406
DLCHAMP <36091350+dlchamp@users.noreply.github.com>
2022-04-04 19:00:21 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # Discord-update-from-csv
- Submit a CSV file  with a list of members and assign them a role
+
+A bot request from a fellow Reddit user
+This bot is designed to take any CSV (see below for formatting requirements) and add a specified role to that list of members.  Popular for server owners that have Google forms and wish to bulk add roles to those members
+
+
+## Getting Started
+
+### Setting up the Bot account
+1. Login to Discord web - https://discord.com
+2. Navigate to Discord Developer Portal - https://discord.com/developers/applications
+3. Click *New Application*
+4. Give the Appplication a name and *Create*
+5. Go to Bot tab and click *Add Bot*
+6. Keep the default settings for Public Bot - *checked* and Require OAuth2 Code Grant - *unchecked*
+7. Under **Priveleged Gateway Intents** enable SERVER MEMBERS INTENT and MESSAGE CONTENT INTENT (required)
+7. Add bot image (optional)
+8. Generate a new token and copy it.  Discord no longer keeps this token available. If you lose it, you will need to generate a new one.
+9. Go to OAuth2 tab > URL Generator
+10. Under *Scopes* - Check Bot
+11. Under *Bot Permissions* - check Manage Roles, Read Messages/View Channels, Send Messages, Read Message History
+12. Copy the generated link and Go to the URL in your browser - Invite Bot to your Discord server
+
+### Configuring the Bot
+Take the copied Token from step 8 above.  Open the .env-sample file in your favorite editor and paste the copied token
+Should read `TOKEN='your pasted token'`.  Save and close
+Rename .env-sample to just `.env`
+
+The bot is now ready to run
+
+
+### Using the Bot
+
+The bot is designed to check on_message events (per request) for a specific csv file being uploaded (members.csv)
+This csv can be as long as necessary, however, it must be formatted accordingly:
+
+
+Column 1 | Discord Name | Column 3 | Column 4| ...
+--- | --- | --- | --- | ---
+Some Data | Username#0000 | Some other data | More data | ...
+
+
+The bot looks for the discord name in column 2.  No matter how many columns or rows your csv file is, discord names must ALWAYS
+be in the 2nd column of data AND your data must have a header row, or risk your first row of values being skipped.
+
+
+Simply update your members.csv to any channel the bot has access to, include the role ID for the role you wish to grant this list of members,
+and press enter.   That's it!
+
+You will either recive a message that ALL members were updated to your submitted role OR
+You will be provided a list of invalid usernames that not found as members in your server.
+
+
+**Notes**
+- Each time a new members.csv is uploaded, the bot's cached version will be replaced
+- Since the bot is not being added with Administrator permissions, though you could if you wanted, it is required that the bot's highest role is higher than the role you wish to grant your csv members.
---
+++ main.py
@@ -0,0 +1,105 @@
+from os import getenv
+from csv import reader
+from json import load
+
+from disnake import Intents, Client
+from disnake.utils import get
+from dotenv import load_dotenv
+
+
+async def get_role(guild, role_id: int):
+    try:
+        return guild.get_role(role_id)
+    except:
+        return None
+
+
+def parse_csv():
+    file = open("./data/members.csv")
+    data = reader(file)
+    next(data)
+    # starts iteration
+    for row in data:
+        yield row[1].split('#')[0].strip()
+
+    file.close()
+
+
+intents = Intents.default()
+intents.members = True
+intents.message_content = True
+bot = Client(intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} is connected to Discord and listening for events.")
+
+
+@bot.event
+async def on_message(message):
+    """listen for message event, only from specific member or roles"""
+
+    # if message author is a bot or doesn't have manage_members permissions, return
+    if (
+        message.author.bot
+        or not message.channel.permissions_for(message.author).manage_roles
+    ):
+        return
+
+
+    guild = message.guild
+
+    bypassed_members = []
+
+    if message.attachments:
+        # if attachments, iterate attachaments and get members.csv
+        for a in message.attachments:
+            if a.filename == "members.csv":
+
+                # attachment exist, check for role ID in message content
+                if message.content:
+
+                    # try to fetch the role object from the role ID included in message
+                    role = await get_role(guild, int(message.content))
+
+                    if role:
+                        # attachment exists, if role also exists, save the member.csv attachment
+                        await a.save("./data/members.csv")
+
+                        # parse the csv and add role to members
+                        for row in parse_csv():
+                            member = get(
+                                guild.members, name=row
+                            )
+
+                            if not member:
+                                bypassed_members.append(row)
+                            else:
+                                if not role in member.roles:
+                                    await member.add_roles(role)
+
+                        if bypassed_members:
+                            bypassed_count = len(bypassed_members)
+                            bypassed_members = "\n".join(bypassed_members)
+
+                            await message.reply(
+                                f"The following {bypassed_count} members were skipped during import, all others were given the {role.name} role:\n{bypassed_members}\n"
+                            )
+
+                        else:
+                            await message.reply(f'All members were updated with the {role.name} role')
+                    # error message sent if role not valid
+                    else:
+                        await message.reply("The submitted role ID is not valid")
+
+                # error message sent if no message included with csv upload
+                else:
+                    await message.reply(
+                        "You must include a role ID when uploading the members.csv file"
+                    )
+
+
+load_dotenv()
+# bot.run(getenv("TOKEN"))
+bot.run('ODc2MTY1MjIyMzU0NzgwMjMw.YRgGcw.7R4TG67i-v0lrd7BbOuCfKaWAZQ')
